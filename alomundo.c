b0bfd33a3194bde0ee0cde1286c7a2af200717ca
FelipeSousaa <67917454+FelipeSousaA@users.noreply.github.com>
2020-07-14 13:39:36 -0300

    Add files via upload
    Primeiro programa em linguagem C

---
+++ alomundo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+	puts("ola, meu nome er FELIPE DE SOUSA AZEVEDO"); /* imprime ola, meu nome er FELIPE DE SOUSA AZEVEDO */
+
+	return 0;
+}
